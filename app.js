3c58aafaac35c11db59aa6ad922ada7f17ec2b05
kharioki <antolisah@gmail.com>
2020-03-22 17:27:39 +0300

    auth routes, added passport, google strategy for passport

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express');
+const authRoutes = require('./routes/auth-routes');
 
 const app = express();
 
 // set up view engine
 app.set('view engine', 'ejs');
 
+// setup routes
+app.use('/auth', authRoutes);
+
 // create home route
 app.get('/', (req, res) => {
     res.render('home');
-})
+});
 
 app.listen(3000, () => {
     console.log('app listening on port 3000!!!');
